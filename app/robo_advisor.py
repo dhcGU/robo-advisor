5c9fbcbc6e504df595b66b59436f86ae7287b25e
dhcGU <46850715+dhcGU@users.noreply.github.com>
2020-02-23 12:32:07 -0500

    formatted output with stock data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+
 
 from dotenv import load_dotenv
 import os
 import requests
+import csv
+import json
 
 invalid_call = """{
     \"Error Message\": \"Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_DAILY.\"
@@ -10,22 +14,7 @@ invalid_call = """{
 load_dotenv()
 API_key = os.environ.get("ALPHAVANTAGE_API_KEY","something isnt right")
 print(API_key) 
-print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("HAPPY INVESTING!") 
-print("-------------------------") 
+
 while True:
     while True:
         symbol = input("Enter a stock ticker to pull information and recommendation: ")
@@ -39,7 +28,35 @@ while True:
         print("Sorry, that was not a stock ticker. Please try again.")
         continue
     break
+parsed_response = json.loads(response.text)
+time_series = parsed_response["Time Series (Daily)"]
+csv_name = "data/" + str(symbol) + "_prices.csv"
+csv_columns = ["open", "high", "low", "close", "volume"]
+f = open(csv_name, 'w+')
+writer = csv.DictWriter(f, fieldnames=csv_columns)
+writer.writeheader()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_day = list(time_series.keys())[0]
+latest_close = float(time_series[latest_day]['4. close'])
+recent_high = float(time_series[latest_day]['2. high'])
+recent_low = float(time_series[latest_day]['3. low'])
+
+
 
 data = response.json()["Time Series (Daily)"]
-print(response.status_code)
-print(response.text)
+print("-------------------------")
+print(f"SELECTED SYMBOL: {symbol}")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: ${latest_close:.2f}")
+print(f"RECENT HIGH: ${recent_high:.2f}")
+print(f"RECENT LOW: ${recent_low:.2f}")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!") 
+print("-------------------------") 

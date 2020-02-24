3c8ee18d724ca0e64bc0e57c9fcdb6217bb761fa
dhcGU <46850715+dhcGU@users.noreply.github.com>
2020-02-23 21:22:51 -0500

    added prediction logic using sklearn

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,6 +7,14 @@ import requests
 import csv
 import json
 from datetime import datetime
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+
+def hasNumber(string):
+    return any(ch.isdigit() for ch in string)
 
 invalid_call = """{
     \"Error Message\": \"Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_DAILY.\"
@@ -18,7 +26,7 @@ API_key = os.environ.get("ALPHAVANTAGE_API_KEY","something isnt right")
 while True:
     while True:
         symbol = input("Enter a stock ticker to pull information and recommendation: ")
-        if(len(symbol) < 1 or len(symbol) > 5):
+        if(len(symbol) < 1 or len(symbol) > 5 or hasNumber(symbol)):
             print("Sorry, that ticker is not valid. Please try again.")
         else:
             break
@@ -48,7 +56,24 @@ with open(csv_file_path, "w+",newline="") as csv_file:
         daily_volume = time_series[key]["5. volume"]
         writer.writerow({"timestamp": key, "open": daily_open, "high": daily_high, "low": daily_low, "close": daily_close, "volume": daily_volume}) 
     csv_file.close()
-    
+
+pandas_data = pd.read_csv(csv_file_path)
+pandas_data['%change'] = (pandas_data['close']-pandas_data['open'])/pandas_data['open']
+x = pandas_data.iloc[:-1,1:6]
+y = pandas_data.iloc[1:,6]
+y_true = pd.Series(y > 0)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y_true, test_size = 0.3, random_state=324)
+increasing_price_classifier = DecisionTreeClassifier(max_leaf_nodes = 15, random_state = 10)
+increasing_price_classifier.fit(x_train,y_train)
+test_predictions = increasing_price_classifier.predict(x_test)
+acc_score = accuracy_score(y_true=y_test, y_pred=test_predictions)
+
+
+to_predict = pandas_data.iloc[1:,1:6]
+real_predictions = increasing_price_classifier.predict(to_predict)
+tomorrows_prediction = real_predictions[-1]
+
     
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 latest_day = list(time_series.keys())[0]
@@ -63,6 +88,9 @@ for key in time_series.keys():
         recent_high = day_high
     if(day_low < recent_low):
         recent_low = day_low
+data = pd.read_csv(csv_file_path)
+print(data)
+
 
 data = response.json()["Time Series (Daily)"]
 print("-------------------------")
